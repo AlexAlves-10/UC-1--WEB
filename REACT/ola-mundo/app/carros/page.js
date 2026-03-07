563fe147afbebab077577fafaa4d0e90e7808a79
AlexAlves-10 <alexalves1099@gmail.com>
2026-03-06 21:52:57 -0300

    att

--- REACT/ola-mundo/app/carros/page.js
+++ REACT/ola-mundo/app/carros/page.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 function Carros() {
 
+    const [exibeListagem, alteraExibeListagem] = useState(true)
+
     const [nome, alteraNome] = useState("")
     const [marca, alteraMarca] = useState("")
     const [valor, alteraValor] = useState()
@@ -11,81 +13,81 @@ function Carros() {
 
     const [carros, alteraListaCarros] = useState(
         [
-        {
-            nome: "Civic",
-            marca: "Honda",
-            cor: "Preto",
-            valor: 145000
-        },
-        {
-            nome: "Corolla",
-            marca: "Toyota",
-            cor: "Branco",
-            valor: 150000
-        },
-        {
-            nome: "Onix",
-            marca: "Chevrolet",
-            cor: "Prata",
-            valor: 85000
-        },
-        {
-            nome: "HB20",
-            marca: "Hyundai",
-            cor: "Vermelho",
-            valor: 82000
-        },
-        {
-            nome: "Gol",
-            marca: "Volkswagen",
-            cor: "Branco",
-            valor: 78000
-        },
-        {
-            nome: "Compass",
-            marca: "Jeep",
-            cor: "Cinza",
-            valor: 185000
-        },
-        {
-            nome: "Renegade",
-            marca: "Jeep",
-            cor: "Preto",
-            valor: 165000
-        },
-        {
-            nome: "Tracker",
-            marca: "Chevrolet",
-            cor: "Azul",
-            valor: 135000
-        },
-        {
-            nome: "Creta",
-            marca: "Hyundai",
-            cor: "Prata",
-            valor: 140000
-        },
-        {
-            nome: "Kicks",
-            marca: "Nissan",
-            cor: "Cinza",
-            valor: 130000
-        },
-       
-    ]
-    ) 
+            {
+                nome: "Civic",
+                marca: "Honda",
+                cor: "Preto",
+                valor: 145000
+            },
+            {
+                nome: "Corolla",
+                marca: "Toyota",
+                cor: "Branco",
+                valor: 150000
+            },
+            {
+                nome: "Onix",
+                marca: "Chevrolet",
+                cor: "Prata",
+                valor: 85000
+            },
+            {
+                nome: "HB20",
+                marca: "Hyundai",
+                cor: "Vermelho",
+                valor: 82000
+            },
+            {
+                nome: "Gol",
+                marca: "Volkswagen",
+                cor: "Branco",
+                valor: 78000
+            },
+            {
+                nome: "Compass",
+                marca: "Jeep",
+                cor: "Cinza",
+                valor: 185000
+            },
+            {
+                nome: "Renegade",
+                marca: "Jeep",
+                cor: "Preto",
+                valor: 165000
+            },
+            {
+                nome: "Tracker",
+                marca: "Chevrolet",
+                cor: "Azul",
+                valor: 135000
+            },
+            {
+                nome: "Creta",
+                marca: "Hyundai",
+                cor: "Prata",
+                valor: 140000
+            },
+            {
+                nome: "Kicks",
+                marca: "Nissan",
+                cor: "Cinza",
+                valor: 130000
+            },
 
-    function salvar(e){
+        ]
+    )
+
+    function salvar(e) {
         e.preventDefault()
-        
+
         const objeto = {
             nome: nome,
             marca: marca,
             cor: "Preto",
             valor: valor
-            
+
         }
-        
+
         //carros.push(objeto)
         alteraListaCarros(carros.concat(objeto))
     }
@@ -94,45 +96,55 @@ function Carros() {
 
         <div >
             <h1> Lista de carros </h1>
+
             <hr />
 
-            <form onSubmit={salvar} >
-
-                <p> Digite o nome do carro: </p>
-                <input onChange={ e => alteraNome(e.target.value) } />
-                <p> Digite a marca do carro: </p>
-                <input onChange={ e => alteraMarca(e.target.value) } />
-                <p> Digite o valor do carro: </p>
-                <input onChange={ e => alteraValor(e.target.value) } />
-
-                <br/><br/>
-                <button>Salvar</button>
-
-            </form>
+            <button onClick={()=> alteraExibeListagem(true)} class="btn btn-primary" >Listagem</button>
+            <button onClick={()=> alteraExibeListagem(false)} class="btn btn-success mx-4" >Cadastro</button>
 
 
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Marca</th>
-                        <th scope="col">Cor</th>
-                        <th scope="col">Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        carros.map(
-                            item => <tr>
-                                <td>{item.nome}</td>
-                                <td>{item.marca}</td>
-                                <td>{item.cor}</td>
-                                <td> R$ {item.valor}</td>
+            {
+                exibeListagem == true ?
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Marca</th>
+                                <th scope="col">Cor</th>
+                                <th scope="col">Valor</th>
                             </tr>
-                        )
-                    }
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody>
+                            {
+                                carros.map(
+                                    item => <tr>
+                                        <td>{item.nome}</td>
+                                        <td>{item.marca}</td>
+                                        <td>{item.cor}</td>
+                                        <td> R$ {item.valor}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                    :
+                    <form onSubmit={salvar} >
+
+                        <p> Digite o nome do carro: </p>
+                        <input onChange={e => alteraNome(e.target.value)} />
+                        <p> Digite a marca do carro: </p>
+                        <input onChange={e => alteraMarca(e.target.value)} />
+                        <p> Digite o valor do carro: </p>
+                        <input onChange={e => alteraValor(e.target.value)} />
+
+                        <br /><br />
+                        <button>Salvar</button>
+
+                    </form>
+
+            }
+
 
 
         </div>
